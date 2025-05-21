4b6a2657ce4a4c40a60ac5674e4e03eb25ab8433
Vladyslav Husenko <qmd4325621@gmail.com>
2025-05-21 15:40:44 +0300

    Added views for project

---
+++ recipe/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe
+from datetime import datetime
+
+
+def main(request):
+    recipes = Recipe.objects.filter(created_at__year=2023)
+    return render(request, 'main.html', {'recipes': recipes})
+
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
